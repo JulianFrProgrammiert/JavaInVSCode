3e0463d407e7bae00c27e59f07eac1d14773879c
Julian F <131391694+JulianFrProgrammiert@users.noreply.github.com>
2024-03-07 11:18:02 +0100

    Java Test Code

---
+++ TestCode/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String hello = "Hello";
+        String name = "Julian";
+        System.out.println(hello + " " + name + "!!!");
+    }
+}
